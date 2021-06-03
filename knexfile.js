d01c3111f4d926ec0f2914a0b1837b94b11f45b7
kendallha <kendallhaw@gmail.com>
2021-06-03 14:04:59 -0700

    Add production instructions

--- knexfile.js
+++ knexfile.js
@@ -12,38 +12,16 @@ module.exports = {
       directory: './db/seeds/dev'
     },
     useNullAsDefault: true
-  }
-
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
-
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+        directory: __dirname + '/db/migrations',
+    },
+    seeds: {
+        directory: __dirname + '/db/seeds/',
+    },
+    useNullAsDefault: true
+  } 
 };
